0be095da692b0ca9df6e9f0c2861b8d96a5f7595
liuyibao <liuyibao@wanda.cn>
2017-11-15 09:02:13 +0800

    update view

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //print_r($_SERVER);
+        //print_r($_FILES);
+        if (isset($_FILES['searchImage']) && $_FILES['error'] == 0) {
+            move_uploaded_file($_FILES['searchImage']['tmp_name'], __DIR__ . '/search/search.jpg');
+            exec('cd ' . __DIR__ .' && /usr/local/bin/python3 search.py');
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -9,15 +16,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <title>相似颜色图像检索</title>
-
-    <!-- Bootstrap -->
     <link href="bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <style type="text/css">
+    .image-row { margin-top: 10px; }
+    .search-result-container { border: 1px #aaa solid; padding: 8px; }
+    </style>
   </head>
   <body>
-    <h1>相似颜色图像检索</h1>
+    <div style="margin: 20px 20px;">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>相似颜色图像检索</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <form method="post" enctype="multipart/form-data">
+                  <div class="form-group">
+                    <label for="searchImage">请上传要检索的图片</label>
+                    <input name="searchImage" type="file" id="searchImage">
+                  </div>
+                  <button type="submit" class="btn btn-default">检索</button>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>上次检索图片</h2>
+                <img src="search/search.jpg" />
+                <h2>上次检索结果</h2>
+            </div>
+        </div>
+        <div class="row search-result-container">
+            <div class="row image-row">
+                <div class="col-md-4">
+                    <img src="result/0.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/1.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/2.jpg" />
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-4">
+                    <img src="result/3.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/4.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/5.jpg" />
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-4">
+                    <img src="result/6.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/7.jpg" />
+                </div>
+                <div class="col-md-4">
+                    <img src="result/8.jpg" />
+                </div>
+            </div>
+            <div class="row image-row">
+                <div class="col-md-4">
+                    <img src="result/9.jpg" />
+                </div>
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="bootstrap-3.3.7/js/jquery.min.js"></script>
